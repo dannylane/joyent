c854e6706e021a739725156870e6744527dbd0a0
Danny Lane <dannylane@gmail.com>
2011-01-05 23:26:59 +0000

    example

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Danny\'s Hello World server in node.js \n');
-}).listen(81, "localhost");
+}).listen(80, "dannylane.no.de");
 console.log('Server running at http://localhost:80');
