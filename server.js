a811a522df389111e6c5d0440425b73333244928
Danny Lane <dannylane@gmail.com>
2011-01-05 23:22:30 +0000

    example

--- server.js
+++ server.js
@@ -1,23 +1,6 @@
-
-
-var log4js = require('./lib/log4js');
 var http = require('http');
-var io = require('./lib/socket.io');
-log4js.addAppender(log4js.consoleAppender());
-log4js.addAppender(log4js.fileAppender('grrr.txt'), 'grrr');
-
-var log = log4js.getLogger('grrr');
-log.setLevel('INFO');
-
-var httpserver = http.createServer(function(req, res){
-	res.writeHeader(200, {'content-type': 'text/plain'});
-	res.writeBody('hello foo');
-	res.finish();
-	
-});
-
-var socket = io.listen(server);
-
-socket.on();
-
-
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Danny\'s Hello World server in node.js \n');
+}).listen(81, "localhost");
+console.log('Server running at http://localhost:80');
