a795366998776fb1a997f0d8cdb58ee5733656ca
Danny Lane <dannylane@gmail.com>
2011-11-20 23:33:58 +0000

    testing

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Danny\'s Hello World server in node.js \n');
+  res.end('Danny\'s Hello World server in node.js. testing.\n');
 }).listen(80, "dannylane.no.de");
 console.log('Server running at http://localhost:80');
